85325c34f4550e5faeaca1f39f335f46e52a26a7
turk-sohail <ksohail470@gmail.com>
2024-02-26 12:32:02 +0500

    c language pointers

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+//   int x[] = {2,3,4,6,7,8};
+//
+//
+//   printf("\n");
+//
+//   for(int i=0;i<6;i++){
+//    printf("%d ",*(x+i));
+//   }
+
+int* j[4];
+int x = 1,y=2,z=3,r=4;
+    j[0] = &x;
+    j[1] = &y;
+    j[2] = &z;
+    j[3] = &r;
+
+
+
+
+//
+//    for(int i=0;i<4;i++){
+//        printf("%lld ",(j+i));
+//    }
+//    printf("\n");
+//
+//    /////////////////
+//
+//    for(int i=0;i<4;i++){
+//        printf("%lld ",*(j+i));
+//    }
+//    printf("\n");
+//
+//    //////////////////
+//
+//    for(int i=0;i<4;i++){
+//        printf("%lld ",**(j+i));
+//    }
+//    printf("\n");
+//
+
+
+
+    return 0;
+}
